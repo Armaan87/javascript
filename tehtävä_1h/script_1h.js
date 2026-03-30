6c8f5cfdb64f0b973b114056e1d5096c6ed4e477
Armaan <Armaan.Sharma@metropolia.fi>
2026-03-30 22:05:20 +0300

    Harjoitustehtävä_1h

---
+++ tehtävä_1h/script_1h.js
@@ -0,0 +1,14 @@
+
+const startYear = Number(prompt("Enter the start year:"));
+const endYear = Number(prompt("Enter the end year:"));
+
+document.write("<ul>");
+
+for (let year = startYear; year <= endYear; year++) {
+
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        document.write("<li>" + year + "</li>");
+    }
+}
+
+document.write("</ul>");
