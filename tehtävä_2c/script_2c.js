f0f94ed287fb27f6204d1785fb02dfe1ce756204
Armaan <Armaan.Sharma@metropolia.fi>
2026-04-01 11:21:47 +0300

    Harjoitustehtävä_2c

---
+++ tehtävä_2c/script_2c.js
@@ -0,0 +1,13 @@
+const dogs = [];
+for (let i = 0; i < 6; i++) {
+    dogs.push(prompt("Enter dog name:"));
+}
+
+dogs.sort();
+dogs.reverse();
+
+document.write("<ul>");
+for (let i = 0; i < dogs.length; i++) {
+    document.write("<li>" + dogs[i] + "</li>");
+}
+document.write("</ul>");
